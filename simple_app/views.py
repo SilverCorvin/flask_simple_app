a5a0c2b8708083fcc36eabe8fa3b849eeadddc03
SilverCorvin <Ivan.Povalyaev@gmail.com>
2017-01-25 01:36:45 +0300

    valid_login function and registration function were complited

--- simple_app/views.py
+++ simple_app/views.py
@@ -1,19 +1,35 @@
-from flask import request, render_template
+from flask import request, render_template, redirect, url_for, escape, session
 
-from simple_app import app
+from simple_app import app, db
+from simple_app.models import UserProfile
+
+
+def valid_login(username, password):
+    """ User login validation function """
+    if username and password:
+        profile = UserProfile.query.filter_by(username=username)
+        if profile and profile.password == password:
+            return True
+
+
+def log_the_user_in(username):
+    """ User login function"""
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return redirect(url_for('login'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if valid_login(request.form['username'], request.form['password']):
-            return log_the_user_in(request.form['username'])
+        username = escape(request.form.get('username'))
+        password = escape(request.form.get('password'))
+        if valid_login(username, password):
+            log_the_user_in(username)
+            return render_template('profile.html')
         else:
             error = 'Invalid username/login'
     return render_template('login.html', error=error)
@@ -21,7 +37,30 @@ def login():
 
 @app.route('/registration', methods=['GET', 'POST'])
 def registration():
-    pass
+    error = None
+    if request.method == 'POST':
+        username = escape(request.form.get('username'))
+        if username and not UserProfile.query.filter_by(username=username):
+            password = escape(request.form.get('password'))
+            password2 = escape(request.form.get('password2'))
+            if password and password == password2:
+                lastname = escape(request.form.get('lastname'))
+                firstname = escape(request.form.get('firstname'))
+                if lastname and firstname:
+                    profile = UserProfile(
+                        username=username,
+                        password=password,
+                        lastname=lastname,
+                        firstname=firstname)
+                    db.session.add(profile)
+                    db.session.commit()
+                else:
+                    error = 'Lastname/Firstname fields are empty'
+            else:
+                error = 'Passwords not match/or passwords fields are empty'
+        else:
+            error = 'Username already exists/or username field is empty'
+    return render_template('registration.html', error=error)
 
 
 @app.route('/logout', methods=['POST'])
@@ -31,4 +70,5 @@ def logout():
 
 @app.route('/profile', methods=['GET', 'POST'])
 def profile():
-    pass
+    # return redirect(url_for('login'))
+    return render_template('profile.html')
