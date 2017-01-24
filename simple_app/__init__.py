62f78631278a749f0c0106cf25939b9b3a3f2954
SilverCorvin <Ivan.Povalyaev@gmail.com>
2017-01-24 23:37:13 +0300

    app config was updated in __init__.py

--- simple_app/__init__.py
+++ simple_app/__init__.py
@@ -1,4 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['DEBUG'] = True
+app.config['TESTING'] = True
+app.config['CSRF_ENABLED'] = True
+app.config['SECRET_KEY'] = 'secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///simple_app/simple_app.db'
+
+db = SQLAlchemy(app)
+
 
 import simple_app.views
+from simple_app.models import UserProfile
