5cd4d478e1db5edac797cb107b28622b3a225bc3
SilverCorvin <Ivan.Povalyaev@gmail.com>
2017-01-24 21:42:56 +0300

    login, registration, logout, profile routes were added to views.py

--- simple_app/views.py
+++ simple_app/views.py
@@ -1,6 +1,34 @@
+from flask import request, render_template
+
 from simple_app import app
 
 
 @app.route('/')
 def index():
     return 'Hello World!'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'], request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/login'
+    return render_template('login.html', error=error)
+
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    pass
+
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    pass
+
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    pass
